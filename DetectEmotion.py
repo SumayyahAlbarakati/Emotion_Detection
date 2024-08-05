c54bf07abb7320e4a57c598b0d3d97d5c9754799
Sumayyah Albarakati <154542003+SumayyahAlbarakati@users.noreply.github.com>
2024-08-05 19:44:29 +0300

    Update DetectEmotion.py

--- DetectEmotion.py
+++ DetectEmotion.py
@@ -29,4 +29,4 @@ def predict():
   return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
-  app.run(debug=True, host='127.0.0.1', port=8080)
+  app.run(debug=True, host='0.0.0.0', port=10000)
